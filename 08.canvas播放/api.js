edec6a512c93d558a8e33213044b49b9a2d1dc29
mohong <mohong@zmn.com>
2021-08-16 17:22:17 +0800

    修改标题

--- 08.canvas播放/api.js
+++ 08.canvas播放/api.js
@@ -1,6 +1,6 @@
 export const addPath = (path) => axios.post('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/add', {
   path,
-  crateTime: Date.now(),
+  createTime: Date.now(),
 });
 
 export const fetchPath = (query) => axios.get('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/get', query).then(res => {
