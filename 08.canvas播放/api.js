b9994be396aaf99f08e183648a60038e01c6ba1e
mohong <mohong@zmn.com>
2021-08-16 19:06:41 +0800

    增加口令

--- 08.canvas播放/api.js
+++ 08.canvas播放/api.js
@@ -1,9 +1,9 @@
-export const addPath = (path) => axios.post('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/add', {
-  path,
+export const addPath = (data) => axios.post('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/add', {
+  ...data,
   createTime: Date.now(),
 });
 
-export const fetchPath = (query) => axios.get('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/get', query).then(res => {
+export const fetchPath = (query) => axios.get('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/get', { params: query }).then(res => {
   if (res.status === 200) {
     return res.data;
   }
