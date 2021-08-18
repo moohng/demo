f03b62380eb1cd366fffc5a0258425f86a84d29f
mohong <mohong@zmn.com>
2021-08-18 17:16:10 +0800

    链接修改

--- 08.canvas播放/api.js
+++ 08.canvas播放/api.js
@@ -1,25 +1,33 @@
+axios.interceptors.request.use((config) => {
+  config.baseURL = 'https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/draw/';
+  config.headers['Content-Type'] = 'application/json';
+  return config;
+});
+
+axios.interceptors.response.use((res) => {
+  if (res.status === 200) {
+    return res.data;
+  }
+  throw new Error(res);
+});
+
 export const addPath = (data) => {
   const hide = tui.Toast.loading('');
-  return axios.post('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/add', {
+  return axios.post('path/add', {
     ...data,
     createTime: Date.now(),
-  }).then(({ status }) => {
-    if (status !== 200) {
-      throw new Error();
-    }
-  }).catch(() => {
+  }).catch((err) => {
     tui.Toast.error('网络开小差了，请重试~');
+    throw new Error(err);
   }).finally(hide);
 };
 
 export const fetchPath = (query) => {
   const hide = tui.Toast.loading('');
-  return axios.get('https://ff827abe-e27d-48d4-a09a-81355a2ce85d.bspapp.com/path/get', { params: query }).then(({ status, data }) => {
-    if (status === 200 && data.data.length) {
-      return data.data[0];
-    }
-    throw new Error(res);
-  }).catch(() => {
+  return axios.get('path/get', { params: query }).then(({ data }) => {
+    return data;
+  }).catch((err) => {
     tui.Toast.error('数据加载失败，请重试~');
+    throw new Error(err);
   }).finally(hide);
 };
