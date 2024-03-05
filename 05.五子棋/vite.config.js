a21b09628acfdc914c4390e21c37d7238feccc04
mohong <mohong@zmn.cn>
2024-03-05 09:17:56 +0800

    更改publicPath目录

--- 05.五子棋/vite.config.js
+++ 05.五子棋/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/wuziqi',
   build: {
     outDir: '../dist/wuziqi',
     emptyOutDir: true,
--- 07.dom操作/vite.config.js
+++ 07.dom操作/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/dom',
   build: {
     outDir: '../dist/dom',
     emptyOutDir: true,
