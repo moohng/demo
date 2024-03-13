2e73a2380f7a7404d29a0cd3a6d54504de153a7d
mohong <mohong@zmn.cn>
2024-03-13 14:37:44 +0800

    自动生成demo数据

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="item in demoList" :key="item.id" :class="item.id">
+      <a :href="item.path">{{ item.name }}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const demoList = __DEMO_LIST__;
+</script>
+
+<style lang="scss" scoped>
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { resolve } from 'path';
-import fs from 'fs/promises';
+import { resolve, join } from 'path';
+import fs from 'fs';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(async () => {
-  const dirs = await fs.readdir(resolve(__dirname, 'demos'))
+  // 获取所有 demo 示例
+  const dirs = fs.readdirSync(resolve(__dirname, 'demos'), { encoding: 'utf-8' });
 
   console.log('===== demos dirs =====', dirs);
 
+  // 打包入口
   const demosInputs = dirs.reduce((inputs, dir) => {
     const dirPath = resolve(__dirname, 'demos', dir, 'index.html');
     return { ...inputs, [dir]: dirPath };
@@ -14,8 +17,30 @@ export default defineConfig(async () => {
 
   console.log('===== demosInputs =====', demosInputs);
 
+  // 生成示例数据
+  const demoList = dirs.map(dir => {
+    const packagePath = resolve(__dirname, 'demos', dir, 'package.json');
+    const routePath = join('/demos', dir, '/');
+    if (fs.existsSync(packagePath)) {
+      const { name, description, author } = JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
+      return {
+        name: name || dir,
+        path: routePath,
+        description,
+        author,
+      };
+    }
+    return {
+      name: dir,
+      path: routePath,
+    };
+  });
+
+  console.log('===== demoList =====', demoList);
+
   return {
     appType: 'mpa',
+    plugins: [vue()],
     build: {
       rollupOptions: {
         input: {
@@ -28,7 +53,9 @@ export default defineConfig(async () => {
     server: {
       host: '0.0.0.0',
       port: 8824,
-      open: true,
+    },
+    define: {
+      __DEMO_LIST__: JSON.stringify(demoList),
     },
   };
 });
