7a975a66e8e7e3235cca30ba72740860e71c3de6
mohong <mohong@zmn.cn>
2024-03-13 11:53:05 +0800

    自动识别demos下的目录

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,34 @@
 import { resolve } from 'path';
+import fs from 'fs/promises';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  // base: './',
-  appType: 'mpa',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        wuziqi: resolve(__dirname, 'demos/wuziqi/index.html'),
-        dotline: resolve(__dirname, 'demos/dotline/index.html'),
-        canvasPlay: resolve(__dirname, 'demos/canvasPlay/index.html'),
+export default defineConfig(async () => {
+  const dirs = await fs.readdir(resolve(__dirname, 'demos'))
+
+  console.log('===== demos dirs =====', dirs);
+
+  const demosInputs = dirs.reduce((inputs, dir) => {
+    const dirPath = resolve(__dirname, 'demos', dir, 'index.html');
+    return { ...inputs, [dir]: dirPath };
+  }, {});
+
+  console.log('===== demosInputs =====', demosInputs);
+
+  return {
+    appType: 'mpa',
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          ...demosInputs,
+        },
       },
+      outDir: 'dist',
     },
-    outDir: 'dist',
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 8824,
-    open: true,
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 8824,
+      open: true,
+    },
+  };
 });
